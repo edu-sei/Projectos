f337f2e0cd267996d5bdba1e3ae06777999b214d
edu-sei <eduseib8@gmail.com>
2025-07-13 23:03:41 -0300

    cambiando unas cosas y probando otras

--- tp03-04/js/index.js
+++ tp03-04/js/index.js
@@ -1,14 +1,24 @@
 const DataResto = './dataResto.json';
+const UrlListGeografica = "https://www.themealdb.com/api/json/v1/1/list.php?a=list";
+const UrlfilterResto = "https://www.themealdb.com/api/json/v1/1/filter.php?a=Mexican";
+const UrlPlato = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772";
 
-function AllRestaurante(){
-    const contenedor = document.getElementById('NombreRestaurante');
+const AllRestaurante = async() => {
+    try{
+        const contenedor = document.getElementById('NombreRestaurante');
 
-    DataResto[0].restaurantes.forEach((item) =>{
-        const NombreResto = document.createElement('h2');
-        NombreResto.textContent = item.nombre.toUpperCase();
-        console.log(NombreResto);
-        contenedor.appendChild(NombreResto);
-    });
-}   
+        const Res = await fetch(UrlListGeografica);
+        const {meals} = await Res.json();
+
+        meals.forEach((item) =>{
+            const NombreResto = document.createElement('h2');
+            NombreResto.textContent = item.strArea.toUpperCase();
+            console.log(NombreResto);
+            
+        });
+    }catch(err){
+        console.error(err);
+    }
+}
 
 AllRestaurante()
